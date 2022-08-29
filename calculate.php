55b334e871a5b3a5736d6799d560f1c94b7c2eea
ahmed <a707a7med@gmail.com>
2022-08-29 17:51:38 +0300

    new calculator

--- calculate.php
+++ calculate.php
@@ -1,6 +1,41 @@
 <?php
 
-echo $_POST["no1"];
-echo $_POST["no2"];
+ $no1 = $_POST["no1"];
+ $no2 = $_POST["no2"];
+
+ $arthematic_operation = $_POST['arth_oper'];
+ 
+/* using if condation 
+ if($arthematic_operation == 'sum_oper') {
+     $cal_val = $no1 + $no2;
+ } else if ($arthematic_operation == 'sub_oper'){
+    $cal_val = $no1 - $no2;
+ } else if ($arthematic_operation == 'multip_oper') {
+    $cal_val = $no1 * $no2;
+ } else {
+    $cal_val = $no1 / $no2;
+ }
+*/
+
+// switch condation 
+switch($arthematic_operation) {
+    case "sum_oper";
+    $cal_val = $no1 + $no2;
+    break;
+    case "sub_oper";
+    $cal_val = $no1 - $no2;
+    break;
+    case "multip_oper";
+    $cal_val = $no1 * $no2;
+    break;
+    default:
+    $cal_val = $no1 / $no2;
+
+
+
+
+}
+echo $cal_val;
+
 
 ?>
--- calculator.php
+++ calculator.php
@@ -15,8 +15,19 @@
               <lable for="no2"> Number 2 : </lable>
                        <input type="number" id="mo2" name="no2">
               </div>
-
+            
               <br>
+              <div>
+                  <label for='arth_oper'> Select Operation : </label>
+                      
+                  <Select id= 'arth_oper' name= 'arth_oper'>
+                      <option value='sum_oper'> Sum + </opton>
+                      <option value='sub_oper'> Sub - </opton>
+                      <option value='multip_oper'> Multiple *</opton>
+                      <option value='divoper'> Div / </opton>
+                </Select>
+             </div>
+                 <br>
               <button id="submit" name="submit" value="Submit"> Calculate </button>
               </form>
 
