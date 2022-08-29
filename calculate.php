a78a46d23c42a6ec8c4c201f5da292e2d5c74009
ahmed <a707a7med@gmail.com>
2022-08-29 11:38:11 +0300

    calculator

---
+++ calculate.php
@@ -0,0 +1,6 @@
+<?php
+
+echo $_POST["no1"];
+echo $_POST["no2"];
+
+?>
---
+++ calculator.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+          <title> Calculator using PHP </title>
+    </head>
+        
+    <body>
+          <h2> Calculator </h2>
+              <form method= "post" action= "calculate.php" name="calform" id="calform">
+              <div>
+                       <lable for="no1"> Number 1 : </lable>
+                       <input type="number" id="no1" name="no1">
+              </div>             
+                 <br> 
+               <div>
+              <lable for="no2"> Number 2 : </lable>
+                       <input type="number" id="mo2" name="no2">
+              </div>
+
+              <br>
+              <button id="submit" name="submit" value="Submit"> Calculate </button>
+              </form>
+
+    </body>
+
+
+</html>
